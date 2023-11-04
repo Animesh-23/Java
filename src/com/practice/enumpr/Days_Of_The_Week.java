55df681594142caa86245ad79f4fb279b9701a7a
Animesh <animeshyesugade2002@gmail.com>
2023-11-04 15:47:43 +0530

    init

---
+++ src/com/practice/enumpr/Days_Of_The_Week.java
@@ -0,0 +1,8 @@
+package com.practice.enumpr;
+
+public class Days_Of_The_Week{
+
+enum Days {
+	SUNDAY,MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY,SATURDAY;
+}
+}
---
+++ src/com/practice/enumpr/Enum.java
@@ -0,0 +1,10 @@
+package com.practice.enumpr;
+
+import com.practice.enumpr.Days_Of_The_Week.Days;
+
+public class Enum {
+	public static void main(String[] args) {
+		Days day = Days.MONDAY;
+		System.out.println(day);
+	}
+}
